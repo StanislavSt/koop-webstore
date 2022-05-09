5ac965a9899dc58fef6c3d70f10cca43222dd1e9
Stanislav Staev <s.staev1@gmail.com>
2022-05-09 11:34:01 +0200

    feat: fetch data from storefront API

--- server/apollo-client.ts
+++ server/apollo-client.ts
@@ -1,7 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
+const httpLink = createHttpLink({
+  uri: process.env.SHOPIFY_STOREFRONT_API_URL,
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN
+  return {
+    headers: {
+      ...headers,
+      'X-Shopify-Storefront-Access-Token': token ? `${token}` : '',
+    },
+  }
+})
 const client = new ApolloClient({
-  uri: 'https://countries.trevorblades.com',
+  ssrMode: typeof window === 'undefined',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
