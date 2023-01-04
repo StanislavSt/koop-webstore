3c05d694ca9baae23be616b2df32a3d9805f3678
Karina Delcheva <karina.t.delcheva@gmail.com>
2023-01-04 22:06:57 +0200

    add getArtistName

--- src/utils/getArtist.ts
+++ src/utils/getArtist.ts
@@ -7,3 +7,11 @@ export const getArtist = (product: Product) => {
     })
   })?.node
 }
+
+export const getArtistName = (product: Product): string | undefined => {
+  return product.collections.edges.find((collection: CollectionEdge) => {
+    return collection.node.metafields.edges.find((field) => {
+      return field.node.key == 'artist' && field.node.value == 'true'
+    })
+  })?.node.title
+}
