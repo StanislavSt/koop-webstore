9bd188ef9f0a31721b5b73fd967b95cfc834b2c9
Stanislav Staev <s.staev1@gmail.com>
2023-01-18 10:53:15 +0100

    test no-cache fetchPolicy for index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { getPlaiceholder } from 'plaiceholder'
 
@@ -17,9 +17,7 @@ export type ProductWithCursor =
     blurDataUrl: string
   }
 
-export const getServerSideProps: GetServerSideProps = async ({
-  locale = 'en',
-}) => {
+export const getStaticProps: GetStaticProps = async ({ locale = 'en' }) => {
   const { data } = await client.query<
     GetProductsQuery,
     GetProductsQueryVariables
