daa397c66c128d55ab37f3901590cd39a1fb8c08
Stanislav Staev <s.staev1@gmail.com>
2023-01-30 16:30:22 +0100

    create announcement static page

---
+++ src/pages/announcement.tsx
@@ -0,0 +1,49 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { GetStaticProps } from 'next/types'
+import AnnouncementPage from '../components/announcement/AnnouncementPage'
+import client from '../graphql/apollo-client-storefront'
+import GetAnnouncement from '../graphql/queries/GetAnnouncement'
+import {
+  GetAnnouncementQuery,
+  GetAnnouncementQueryVariables,
+  LanguageCode,
+} from '../graphql/types'
+import { getPlaiceholder } from 'plaiceholder'
+
+export default AnnouncementPage
+
+export const getStaticProps: GetStaticProps = async ({ locale = 'en' }) => {
+  const { data } = await client.query<
+    GetAnnouncementQuery,
+    GetAnnouncementQueryVariables
+  >({
+    query: GetAnnouncement,
+    variables: {
+      collectionId: 'gid://shopify/Collection/276785594504',
+      language: locale === 'bg' ? LanguageCode.Bg : LanguageCode.En,
+    },
+  })
+  const productsWithCursor =
+    data.collection &&
+    (await Promise.all(
+      data.collection.products.edges.map(async (edge) => {
+        const blurDataURL = await (
+          await getPlaiceholder(edge.node.images.edges[0].node.placeholder)
+        ).base64
+        return {
+          ...edge.node,
+          cursor: edge.cursor,
+          blurDataUrl: blurDataURL,
+        }
+      })
+    ))
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+      announcement: data.collection,
+      products: productsWithCursor,
+    },
+    revalidate: 10,
+  }
+}
