fc4c32ff6e840e43d62fd86615d05717f15873af
Stanislav Staev <s.staev1@gmail.com>
2023-02-23 11:06:15 +0100

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { ApolloProvider } from '@apollo/client/react/context/ApolloProvider'
+import { Analytics } from '@vercel/analytics/react'
 
 import client from '../graphql/apollo-client-storefront'
 
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
+      <Analytics />
     </ApolloProvider>
   )
 }
